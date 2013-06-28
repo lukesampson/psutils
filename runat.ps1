9df60d53c52215737ce4a78899ee75a3b7f2240e
Luke Sampson <me@lukesampson.com>
2013-06-28 22:15:53 +1000

    initial commit

---
+++ runat.ps1
@@ -0,0 +1,36 @@
+$usage = "usage: runat (time) (command)
+    
+e.g. runat 2am shutdown -r -f -c ""restart""";
+
+function esc($s) { [regex]::escape($s) }
+
+$t = $args[0] # time
+
+if($args.length -gt 1) { # command
+  $c = $args[1..($args.length-1)] | % { if($_ -match '\s') { "`"$_`"" } else { $_ } }
+  $c = [string]::join(' ', $c)
+}
+
+try { $d = get-date $t } # parse time
+catch { echo "error: invalid time: '$t'"; $usage; exit 1 }
+
+if($d -lt [DateTime]::Now) { $d = $d.AddDays(1) }
+if($d -lt [DateTime]::Now) { echo "error: invalid time: '$t'"; $usage; exit 1 }
+
+if(!$d -or !$c) { $usage; exit 1; }
+
+# /query for unused task name (returns exit code 1 when not found)
+$tn='';
+for($i=1;$true;$i++) {
+    & schtasks /query /tn "runat$i" 2>&1 | out-null
+    if($lastexitcode -eq 1) { $tn = "runat$i"; break }
+}
+
+# coerce system date format for schtasks, doubling-up 'd' and 'M'
+$df = (get-culture).datetimeformat.shortdatepattern -replace '(?<![Md])(M|d)(?!\1)', '$1$1'
+
+# create the task
+& schtasks /create /ru system /rl highest /sc once /tn $tn /tr `"$($c -replace '"', '\"')`" /st $d.tostring("hh:mm") /sd $d.tostring($df)
+
+if($lastexitcode -eq 0) { echo "Task will run '$c' at $d"; exit 0 }
+exit 1;
---
+++ vimtutor.ps1
@@ -0,0 +1,34 @@
+# Starts Vim on a copy of the tutor file.
+# Usage: vimtutor [xx]
+#
+# xx is a language code like "es" or "nl".
+# When no xx argument is supplied, it will use the current UI culture.
+#
+# Adapted from the vimtutor.bat included with Vim for Windows, modified to
+# work with Scoop in Powershell
+param($xx)
+
+if(!$xx) { $xx = (get-uiculture).twoLetterISOLanguageName }
+
+$vim = scoop which vim
+if(!$vim) {
+    try { $vim = gcm vim -ea 0 } catch { }
+    "vim isn't installed."; exit 1
+}
+
+$vimdir = split-path $vim
+
+$tutorcopy = "$env:temp\`$tutor`$"
+
+# use environment variables so that Vim can access them
+$env:tutorcopy = $tutorcopy # where to copy the tutor file to
+$env:xx = $xx               # language code
+
+# tutor.vim works out which tutor file to use (based on $env:xx)
+# and then copies it to $env:tutorcopy
+vim -u NONE -c "so $vimdir\tutor\tutor.vim"
+
+# Start vim without any .vimrc, set 'nocompatible'
+vim -u NONE -c "set nocp" "$tutorcopy"
+
+if(test-path $tutorcopy) { rm $tutorcopy }
