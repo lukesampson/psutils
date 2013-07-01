e2d4737e400942e0495b05bc3965ad51fd44b2fc
Luke Sampson <me@lukesampson.com>
2013-07-01 23:39:28 +1000

    got date parsing working for touch

--- touch.ps1
+++ touch.ps1
@@ -1,10 +1,10 @@
 $usage = "usage:
 touch [-A [-][[hh]mm]SS] [-acm] [-r file] [-t [[CC]YY]MMDDhhmm[.SS]] file ..."
 
-$flags  = 'acm'.tochararray() # defines options without a parameter
-$pflags = 'Art'.tochararray() # defines options with a parameter
-$opts   = @{}                 # stores parsed options
-$files  = @()                 # stored parsed files
+$flags  = 'acm'.tochararray()              # defines options without a parameter
+$pflags = 'Art'.tochararray()              # defines options with a parameter
+$opts   = new-object collections.hashtable # stores parsed options
+$files  = @()                              # stored parsed files
 
 function dbg($msg) { write-host $msg -f darkyellow }  # temp debugging
 
@@ -19,13 +19,13 @@ for($i = 0; $i -lt $args.length; $i++) {
             if($i -eq $args.length - 1) {
                 "touch: $flag requires a parameter"; $usage; exit 1
             }
-            $opts[$arg[1]] = $args[++$i]
+            $opts[[string]$arg[1]] = $args[++$i]
         } elseif($flags -ccontains $flag) {
             # flag(s) with no parameters (may be grouped together e.g. -amc)
-            $opts[$flag] = $true
+            $opts[[string]$flag] = $true
             for($j = 2; $j -lt $arg.length; $j++) {
                 $flag = $arg[$j]
-                if($flags -ccontains $flag) { $opts[$flag] = $true }
+                if($flags -ccontains $flag) { $opts[[string]$flag] = $true }
                 else {
                     "touch: illegal option -- $flag"; $usage; exit 1
                 }
@@ -38,22 +38,47 @@ for($i = 0; $i -lt $args.length; $i++) {
     }
 }
 
-dbg 'first pass:'
-$opts
-
-$opts.A = 'test'
-
-dbg "parsing $($opts.A)..."
 # parse pflags parameters
-if($opts.A) {
-    dbg 'opts.A'
+if($opts.A) { 
     $format = "[-][[hh]mm]SS"
-    if($opts.A -match '(?<neg>-?)((?<hh>\d{2})?(?<mm>\d{2}))?(?<SS>\d{2})') {
+    if($opts.A -match '^(?<neg>-?)((?<hh>\d{2})?(?<mm>\d{2}))?(?<SS>\d{2})$') {
         $s = 0;
-        if($matches.hh) { $s += int.parse($matches.hh) * 3600 }
-        if($matches.mm) { $s += int.parse($matches.mm) * 60 }
-        if($matches.SS) { $s += int.parse($matches.SS) }
-        dbg $s
+        if($matches.hh) { $s += [int]::parse($matches.hh) * 3600 }
+        if($matches.mm) { $s += [int]::parse($matches.mm) * 60 }
+        if($matches.SS) { $s += [int]::parse($matches.SS) }
+        if($matches.neg) { $s = -$s }
+        $opts.A = [timespan]::fromseconds($s)
     } else { "touch: invalid offset spec for A, must be $format" }
 }
+
+if($opts.t) {
+    $format = "[[CC]YY]MMDDhhmm[.SS]"
+    $err = "touch: out of range or illegal time specification: $format"
+    if($opts.t -match '^((?<CC>\d{2})?(?<YY>\d{2}))?(?<MDhm>\d{8})(?:\.(?<SS>\d{2}))?$') {
+        $d = @{} # store date parts
+        @('CC','YY','MDhm','SS') | % { 
+            $val = 0;
+            [int]::tryparse($matches.$_, [ref]$val); $d.$_ = $val;
+        }
+        # fill in missing values
+        $now = [datetime]::now
+        if(!$d.yy) {
+            $d.yy = $now.year % 1000; $d.cc = [int][math]::floor($now.year / 100)
+        } elseif(!$d.cc) {
+            if(($d.yy -gt 69) -and ($d.yy -lt 99)) { $d.cc = 19 } else { $d.cc = 20 }
+        }
+        $datef = "{0:d2}{1:d2}{2:d8}.{3:d2}" -f $d.cc, $d.yy, $d.mdhm, $d.ss
+        dbg $datef
+        $culture = [cultureinfo]::invariantculture
+        try {
+            $opts.t = [datetime]::parseexact($datef, "yyyyMMddHHmm.ss", $culture)
+        } catch {
+            $err; exit 1;
+        }
+    } else { $err; exit }
+}
+
+$opts
+
+exit 0
 
