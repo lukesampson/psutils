e3c78f2d4e560a57bfeae33ed9db5b06f9b31b60
Luke Sampson <me@lukesampson.com>
2013-07-12 21:22:27 +1000

    sudo: handle no command supplied

--- sudo.ps1
+++ sudo.ps1
@@ -1,3 +1,5 @@
+if(!$args) { "usage: sudo <cmd...>"; exit 1 }
+
 $a = $args | % { if($_ -match '\s') { "`"$_`""} else { $_ } }
 $a = [string]::join(' ', $a)
 $prompt = 'write-host ''press any key to close this window...'' -nonewline; $null = $host.ui.rawui.readkey(''NoEcho,IncludeKeyDown'')'
