8d111647b3100bd8c821405fcefacf783a8dbe94
Luke Sampson <me@lukesampson.com>
2013-09-17 21:27:54 +1000

    more sudo testing

--- test/sudo.ps1
+++ test/sudo.ps1
@@ -1,14 +1,22 @@
 ls "$psscriptroot\sudo\" '*.in' | % {
 	$name = $_.name -replace '\.in$', ''
 	$path = $_.fullname
-	$expect = gc ($path -replace '\.in$', '.out')
+	$outpath = $path -replace '\.in$', '.out'
+	if(test-path $outpath) { $expect = gc ($outpath) }
 
 	$arguments = gc $path
-	$out = & "$psscriptroot\..\sudo.ps1" $arguments
+	$out = iex "$psscriptroot\..\sudo.ps1 $arguments"
 
-	$diff = compare-object $expect $out
-	$eq = !$diff
-	write-host "$name`: $eq"
+	write-host "$name`: " -nonewline
+	if($expect) {
+		$diff = compare-object $expect $out
+		$eq = !$diff
+		if($eq) { write-host "pass" -f darkgreen }
+		else { write-host "fail" -f darkred }
+	} else {
+		write-host "error: no .out file" -f darkyellow
+	}
+	
 	if(!$eq) {
 		$diff
 	}
