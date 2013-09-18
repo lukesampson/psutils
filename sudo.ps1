e03d8dbedc58f79ff6ce641cfaebb5ed16a24d19
Luke Sampson <me@lukesampson.com>
2013-09-18 14:59:21 +1000

    if already elevated, just run the command. strip empty lines from error output

--- sudo.ps1
+++ sudo.ps1
@@ -1,5 +1,10 @@
 if(!$args) { "usage: sudo <cmd...>"; exit 1 }
 
+function is_elevated {
+	$id = [Security.Principal.WindowsIdentity]::GetCurrent()
+	([Security.Principal.WindowsPrincipal]($id)).isinrole("Administrators")
+}
+
 function sudo_do($parent_pid, $dir, $cmd) {
 	$a = @($a)
 	$c = new-object io.pipes.namedpipeclientstream '.', "/tmp/sudo/$parent_pid", 'out', 'none', 'anonymous'
@@ -9,14 +14,15 @@ function sudo_do($parent_pid, $dir, $cmd) {
 		
 		$p = new-object diagnostics.process; $start = $p.startinfo
 		$start.filename = "powershell.exe"
-		write-host "cmd: $cmd"
-		$start.arguments = "-nologo $cmd;exit `$lastexitcode"
+		$start.arguments = "-noprofile -nologo $cmd`nexit `$lastexitcode"
 		$start.useshellexecute = $false
 		$start.redirectstandardoutput = $true
 		$start.redirectstandarderror = $true
 		$start.workingdirectory = $dir
 		register-objectevent $p errordatareceived -action {
-			$global:sw.writeline("2$($eventargs.data)"); $global:sw.flush();
+			if($err = $eventargs.data) {
+				$global:sw.writeline("2$err"); $global:sw.flush();
+			}
 		}
 		$p.start()
 
@@ -26,7 +32,8 @@ function sudo_do($parent_pid, $dir, $cmd) {
 			$sw.writeline("1$line"); $sw.flush()
 		}
 		$p.waitforexit()
-		write-host "" -nonewline # for some reason, errors aren't written without this
+		write-host "" -nonewline
+		write-host "" -nonewline # for some reason, errors aren't written fully without these
 		$sw.writeline("0$($p.exitcode)"); $sw.flush() # can't seem to access exitcode for process started with runas
 	} finally {
 		if($sw) { $sw.flush() }
@@ -45,7 +52,13 @@ function serialize($a) {
 if($args[0] -eq '-do') {
 	$_, $dir, $parent_pid, $cmd = $args
 	$cmd = serialize $cmd
-	sudo_do $parent_pid $dir $cmd
+	$null = sudo_do $parent_pid $dir $cmd
+	exit
+}
+
+if(is_elevated) {
+	$cmd, $args = @($args)
+	& $cmd @($args)
 	exit
 }
 
@@ -54,7 +67,7 @@ $a = serialize $args
 $exitcode = 0
 $s = new-object io.pipes.namedpipeserverstream "/tmp/sudo/$pid", 'in'
 try {
-	$p = start powershell.exe -arg "-nologo -window minimized & '$pscommandpath' -do $pwd $pid $a" -verb runas -passthru
+	$p = start powershell.exe -arg "-noprofile -nologo -window minimized & '$pscommandpath' -do $pwd $pid $a" -verb runas -passthru
 	$s.waitforconnection()
 	$sr = new-object io.streamreader $s
 	$line = $null
