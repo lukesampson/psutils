83ae7d3851bf5aa0c514c144b3a64500e250a659
Luke Sampson <me@lukesampson.com>
2013-09-30 18:09:10 +1000

    start do-over

--- sudo.ps1
+++ sudo.ps1
@@ -2,50 +2,28 @@ if(!$args) { "usage: sudo <cmd...>"; exit 1 }
 
 $id = [Security.Principal.WindowsIdentity]::GetCurrent()
 
-function is_elevated {	
-	([Security.Principal.WindowsPrincipal]($id)).isinrole("Administrators")
-}
-
-function is_admin {
-	$name = $id.name -replace '^[^\\]*\\', ''
-	$res = gwmi win32_groupuser | ? { $_.groupcomponent -match 'name="administrators"' -and $_.partcomponent -match "name=`"$name`"" }
-	if($res) { $true }
-}
-
 function sudo_do($parent_pid, $dir, $cmd) {
-	$a = @($a)
-	$c = new-object io.pipes.namedpipeclientstream '.', "/tmp/sudo/$parent_pid", 'out', 'none', 'anonymous'
-	try {
-		$c.connect()
-		$global:sw = new-object io.streamwriter $c # global so event handler can access
-		
-		$p = new-object diagnostics.process; $start = $p.startinfo
-		$start.filename = "powershell.exe"
-		$start.arguments = "-noprofile -nologo $cmd`nexit `$lastexitcode"
-		$start.useshellexecute = $false
-		$start.redirectstandardoutput = $true
-		$start.redirectstandarderror = $true
-		$start.workingdirectory = $dir
-		register-objectevent $p errordatareceived -action {
-			if($err = $eventargs.data) {
-				$global:sw.writeline("2$err"); $global:sw.flush();
-			}
-		}
-		$p.start()
+	$src = 'using System.Runtime.InteropServices;
+	public class Kernel {
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool AttachConsole(uint dwProcessId);
 
-		$p.beginerrorreadline()
-		$line = $null
-		while($line = $p.standardoutput.readline()) {
-			$sw.writeline("1$line"); $sw.flush()
-		}
-		$p.waitforexit()
-		write-host "" -nonewline
-		write-host "" -nonewline # for some reason, errors aren't written fully without these
-		$sw.writeline("0$($p.exitcode)"); $sw.flush() # can't seem to access exitcode for process started with runas
-	} finally {
-		if($sw) { $sw.flush() }
-		if($c -and $c.isconnected) { $c.dispose() }
-	}
+		[DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+		public static extern bool FreeConsole();
+	}'
+
+	$kernel = add-type $src -passthru
+
+	$kernel::freeconsole()
+	$kernel::attachconsole($parent_pid)
+		
+	$p = new-object diagnostics.process; $start = $p.startinfo
+	$start.filename = "powershell.exe"
+	$start.arguments = "-noprofile $cmd`nexit `$lastexitcode"
+	$start.useshellexecute = $false
+	$start.workingdirectory = $dir
+	$p.start()
+	$p.waitforexit()
 }
 
 function serialize($a) {
@@ -58,43 +36,16 @@ function serialize($a) {
 
 if($args[0] -eq '-do') {
 	$_, $dir, $parent_pid, $cmd = $args
-	$cmd = serialize $cmd
-	$null = sudo_do $parent_pid $dir $cmd
+	$null = sudo_do $parent_pid $dir (serialize $cmd)
 	exit
-}
-
-if(is_elevated) {
-	$cmd, $args = @($args)
-	& $cmd @($args)
-	exit $lastexitcode
-}
-
-if(!(is_admin)) {
-	[console]::error.writeline("sudo: you must be an administrator to run sudo")
-	exit 1
 }
 
 $a = serialize $args
 
-$exitcode = 0
-$s = new-object io.pipes.namedpipeserverstream "/tmp/sudo/$pid", 'in'
-try {
-	$p = start powershell.exe -arg "-noprofile -nologo -window minimized & '$pscommandpath' -do $pwd $pid $a" -verb runas -passthru
-	$s.waitforconnection()
-	$sr = new-object io.streamreader $s
-	$line = $null
-	while($line = $sr.readline()) {
-		$stream = $line[0]
-		$line = $line.substring(1)
-		if($stream -eq '1') { $line }
-		elseif($stream -eq '2') { [console]::error.writeline($line) }
-		elseif($stream -eq '0') { $exitcode = [int]$line }
-		else { "$stream$line" }
-	}
-} catch [InvalidOperationException] {
-	# user didn't provide consent: ignore
-} finally {
-	if($s) { $s.dispose() }
-}
-
-exit $exitcode
+$p = new-object diagnostics.process; $start = $p.startinfo
+$start.filename = "powershell.exe"
+$start.arguments = "-noprofile -noexit & '$pscommandpath' -do $pwd $pid $a"
+$start.verb = 'runas'
+$start.windowstyle = 'hidden'
+$started = $p.start()
+$p.waitforexit()
